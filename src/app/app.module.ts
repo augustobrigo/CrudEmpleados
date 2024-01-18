d67136f57c5b4d5ded399bef2f87d59b9da172e1
augustobrigo <profeaugustobriga@gmail.com>
2024-01-18 14:27:55 +0100

    ss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CuadroDialogoEmpleadoComponent } from './cuadro-dialogo-empleado/cuadro-dialogo-empleado.component';
+import { ListadoComponent } from './listado/listado.component';
+import { FormsModule } from '@angular/forms';
+import{HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CuadroDialogoEmpleadoComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,FormsModule,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
